2fa2cf1e6e65cfa4c5a9f912039f883b4d82c3ee
Anders Hansen-Haug <andershh_93@hotmail.com>
2021-10-13 15:24:36 +0200

    Fixed fetch API method so it works now! Yippie!

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const square = document.querySelector('#square');
-const lista = document.querySelector('lista');
-
+const lista = document.querySelector('.lista');
 
 const fetchData = async () => {
-    const res = await fetch(`https://www.flickr.com/explore`);
-    
-    const result = res.json();
 
-    renderData(result);
+    const res = await fetch('https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=fae3b5188605557014e24bd343d2256e&gallery_id=72157719791154473&format=json&nojsoncallback=1');
+    
+    const result = await res.json();
+
+    renderData(result.photos.photo);
 }
 
 
 const renderData = data => {
     data.map(value =>  {
-
-        const li = document.querySelector('li');
-        li.innerHTML = `${value.id}`;
-        lista.appendChild(li);
+        console.log(value);
+        const item = document.createElement('li');
+        item.innerHTML = `<img src="https://live.staticflickr.com/${value.server}/${value.id}_${value.secret}_w.jpg">`;
+        lista.appendChild(item);
     });
 }
+
+fetchData();
